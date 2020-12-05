f7cbfe56d88d6d0a57b8be10fc3284f041aa9b4c
yasuaki640 <42242014+yasuaki640@users.noreply.github.com>
2020-12-05 10:51:51 +0900

    create todo names by js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
+  // create todo tags
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  div.appendChild(li);
+
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
